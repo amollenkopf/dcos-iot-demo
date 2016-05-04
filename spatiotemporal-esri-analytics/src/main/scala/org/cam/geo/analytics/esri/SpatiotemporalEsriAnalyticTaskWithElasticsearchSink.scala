8216c817d328918f9bf161885c347c362ade5a74
Adam Mollenkopf <amollenkopf@esri.com>
2016-05-04 10:26:43 -0700

    Made esNode and esClusterName configurable.

--- spatiotemporal-esri-analytics/src/main/scala/org/cam/geo/analytics/esri/SpatiotemporalEsriAnalyticTaskWithElasticsearchSink.scala
+++ spatiotemporal-esri-analytics/src/main/scala/org/cam/geo/analytics/esri/SpatiotemporalEsriAnalyticTaskWithElasticsearchSink.scala
@@ -12,32 +12,35 @@ import org.codehaus.jackson.JsonFactory
 import org.elasticsearch.spark._
 
 object SpatiotemporalEsriAnalyticTaskWithElasticsearchSink {
-  //TODO: docker run -it -p 9000:9000 badmf -DES_NODES=localhost -DES_PORT=9300 -DES_CLUSTER_NAME=spatiotemporal-store
+  //TODO: esri-geo-hash ---geo_hash---
   //TODO: refactor geofence ring to common AnalyticData object or file
+  //TODO: /arcgis/rest/services/test2/FeatureServer/0/query?where=1%3D1&objectIds=
+  //TODO:   lod=15  lodType=pointyHexagon lodSR=102100 returnLodGeometry=true f=pjson
   val geofenceRing =
     "{\"rings\":[[[-117.16371744999998,33.52359333600003],[-117.15562728199995,33.529304042000035],[-117.14967862999998,33.533587072000046],[-117.14325408499997,33.54191518500005],[-117.13540186399996,33.53929777800005],[-117.12350455899997,33.541201347000026],[-117.12041125899998,33.55405043700006],[-117.08376755999996,33.55428838300003],[-117.08400550599998,33.54048750900006],[-117.10089967899995,33.54024956300003],[-117.10018584099998,33.527876366000044],[-117.09471307999996,33.51978619800008],[-117.08900237399996,33.51026835400006],[-117.08091220699998,33.512885761000064],[-117.07091846999998,33.49385007300003],[-117.08733675099995,33.48385633700008],[-117.10518270899996,33.47885946900004],[-117.11636617599999,33.47576616900005],[-117.12493223499996,33.47671795400004],[-117.13016704899997,33.475528223000026],[-117.14111256999996,33.48100098400005],[-117.14539559999997,33.493136235000065],[-117.15086835999995,33.499560780000024],[-117.15753085099999,33.51240986900007],[-117.16371744999998,33.52359333600003]]],\"spatialReference\": {\"wkid\":4326}}"
   val geofence = GeometryEngine.jsonToGeometry(new JsonFactory().createJsonParser(geofenceRing)).getGeometry
 
   def main(args: Array[String]) {
-    if (args.length < 5) {
-      System.err.println("Usage: EsriSpatiotemporalAnalyticTask <zkQuorum> <topic(s)> <consumerGroupId> <geofenceFilteringOn>")
+    if (args.length < 7) {
+      System.err.println("Usage: EsriSpatiotemporalAnalyticTask <zkQuorum> <topic(s)> <consumerGroupId> <geofenceFilteringOn> <esNode> <esClusterName>")
       System.err.println("          zkQuorum(s): the zookeeper url, e.g. localhost:2181")
       System.err.println("             topic(s): a comma separated list of the Kafka topic(s) name to consume from, e.g. source01")
       System.err.println("      consumerGroupId: the Kafka consumer group id to consume with, e.g. source01-consumer-id")
       System.err.println("  geofenceFilteringOn: indicates whether or not to apply a geofence filter, e.g. true")
       System.err.println("             stdoutOn: indicates whether or not to write to stdout, e.g. true")
+      System.err.println("               esNode: the hostname and port of the Elasticsearch cluster, e.g. localhost:9200")
+      System.err.println("        esClusterName: the name of the Elasticsearch cluster, e.g. spatiotemporal-store")
       System.exit(1)
     }
     AnalyticLog.setStreamingLogLevels()
 
-    val Array(zkQuorum, topics, consumerGroupId, geofenceFilteringOnStr, stdoutOnStr) = args
+    val Array(zkQuorum, topics, consumerGroupId, geofenceFilteringOnStr, stdoutOnStr, esNodes, esClusterName) = args
     val geofenceFilteringOn = geofenceFilteringOnStr.toBoolean
     val stdoutOn = stdoutOnStr.toBoolean
-    val esNodes = "localhost:9200"
 
     val sparkConf = new SparkConf()
       .setAppName("spatiotemporal-esri-analytic-task-with-elasticsearch-sink")
-      .set("es.cluster.name", "spatiotemporal-store")
+      .set("es.cluster.name", esClusterName)
       .set("es.nodes", esNodes)
       .set("es.index.auto.create", "true")
 
